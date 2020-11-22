0ed74163dc6c5a5a33a42fc62222d065cfec78b4
nnyimc <nmncls@orange.fr>
2020-11-22 22:20:02 +0100

    Implementing dynamic load balancing - OK

--- src/main/java/fr/nnyimc/api_gateway/ApiGatewayApplication.java
+++ src/main/java/fr/nnyimc/api_gateway/ApiGatewayApplication.java
@@ -2,13 +2,31 @@ package fr.nnyimc.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.config.conditional.ConditionalOnEnabledGlobalFilter;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+	
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(
+			ReactiveDiscoveryClient rdc, 
+			DiscoveryLocatorProperties dlp
+			) 
+	{
+		return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
 	}
 
 }
