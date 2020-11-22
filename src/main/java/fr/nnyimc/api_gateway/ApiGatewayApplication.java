b5d492d55bc18b2bad3c4a302918f6a3cad61754
nnyimc <nmncls@orange.fr>
2020-11-22 23:12:58 +0100

    Updating pom.xml - dynamic config OK

--- src/main/java/fr/nnyimc/api_gateway/ApiGatewayApplication.java
+++ src/main/java/fr/nnyimc/api_gateway/ApiGatewayApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
-import org.springframework.cloud.gateway.config.conditional.ConditionalOnEnabledGlobalFilter;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 
